f73ac9ec497f7a52f9cc67b39ef73a2c4e1db4f8
Benjamin Jakimow benjamin.jakimow@geo.hu-berlin.de <benjamin.jakimow@geo.hu-berlin.de>
2023-03-09 15:44:14 +0100

    create_namensschilder.bat damit pdflatex schneller ist

---
+++ namensschilder/create_namensschilder.bat
@@ -0,0 +1,7 @@
+:: diese script erstellt die A4 Badge Blätter vorder (namensschilder_innenseite.tex -> pdf) und
+:: Rückseiten (namensschilder_innenseite.tex -> pdf)
+:: CSV -> die CSV datei mit ggf. manuell korrigierten Namen
+
+set CSV=bin/badges.proof.csv
+pdflatex "\newcommand{\BadgeCSV}{%CSV%} \input{namensschilder_innenseite.tex}"
+pdflatex "\newcommand{\BadgeCSV}{%CSV%} \input{namensschilder_sichtbar.tex}"
